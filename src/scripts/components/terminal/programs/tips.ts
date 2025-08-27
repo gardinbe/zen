12e397a46a2ee57167e4afe42e4882e7bf363786
Ben Gardiner <gardinbe@gmail.com>
2025-08-27 12:12:21 +0100

    updates

--- src/scripts/components/terminal/programs/tips.ts
+++ src/scripts/components/terminal/programs/tips.ts
@@ -14,7 +14,8 @@ export const TipsProgram: ProgramConstructor = {
       const str =
         '[[insert:\n]]Tips:\n\n' +
         '<ul class="u-no-margin">' +
-        '<li>Hold <kbd>CTRL</kbd> before pressing start to return to the terminal (if on mobile, hold down the start button and shake your device abruptly).</li>' +
+        '<li>Hold <kbd>CTRL</kbd> before pressing start to return to the terminal.</li>' +
+        '<li>If on mobile, hold down the <strong>Start</strong> button and shake your device abruptly.</li>' +
         '<li>Press <kbd>Tab</kbd>/<kbd>Shift+Tab</kbd> to cycle through suggestions.</li>' +
         '<li>Press <kbd>Up</kbd>/<kbd>Down</kbd> to navigate through the command history.</li>' +
         '<li>Press <kbd>CTRL+C</kbd> to terminate a running program.</li>' +
